605fff5c28f5745df814665c5fb8545930e1f2ff
komeghan <komeghan@INLEN8520086786.corp.capgemini.com>
2023-02-15 09:03:21 +0530

    1st cmt

---
+++ src/main/java/com/projectmanagement/entity/Project.java
@@ -0,0 +1,143 @@
+package com.projectmanagement.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+
+@Entity
+
+public class Project {
+	@Id
+	private long projectId;
+
+	private String projectName;
+
+	private String projectCode;
+	private String projectBudget;
+
+	private String technicalRequirments;
+
+	private String projectManagerName;
+
+	private String projectTechnology;
+
+	private String projectCoordinator;
+	
+	private String projectDiscription;
+
+	@Enumerated(EnumType.STRING)
+	private Status status;
+
+	@OneToOne(mappedBy = "project", cascade = CascadeType.ALL)
+	@JoinColumn(name="project_fk")
+	private Team team;
+
+	public long getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(long projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getProjectCode() {
+		return projectCode;
+	}
+
+	public void setProjectCode(String projectCode) {
+		this.projectCode = projectCode;
+	}
+
+	public String getProjectBudget() {
+		return projectBudget;
+	}
+
+	public void setProjectBudget(String projectBudget) {
+		this.projectBudget = projectBudget;
+	}
+
+	public String getTechnicalRequirments() {
+		return technicalRequirments;
+	}
+
+	public void setTechnicalRequirments(String technicalRequirments) {
+		this.technicalRequirments = technicalRequirments;
+	}
+
+	public String getProjectManagerName() {
+		return projectManagerName;
+	}
+
+	public void setProjectManagerName(String projectManagerName) {
+		this.projectManagerName = projectManagerName;
+	}
+
+	public String getProjectTechnology() {
+		return projectTechnology;
+	}
+
+	public void setProjectTechnology(String projectTechnology) {
+		this.projectTechnology = projectTechnology;
+	}
+
+	public String getProjectCoordinator() {
+		return projectCoordinator;
+	}
+
+	public void setProjectCoordinator(String projectCoordinator) {
+		this.projectCoordinator = projectCoordinator;
+	}
+
+	public String getProjectDiscription() {
+		return projectDiscription;
+	}
+
+	public void setProjectDiscription(String projectDiscription) {
+		this.projectDiscription = projectDiscription;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+
+	@Override
+	public String toString() {
+		return "Project [projectId=" + projectId + ", projectName=" + projectName + ", projectCode=" + projectCode
+				+ ", projectBudget=" + projectBudget + ", technicalRequirments=" + technicalRequirments
+				+ ", projectManagerName=" + projectManagerName + ", projectTechnology=" + projectTechnology
+				+ ", projectCoordinator=" + projectCoordinator + ", projectDiscription=" + projectDiscription
+				+ ", status=" + status + ", team=" + team + "]";
+	}
+	
+	
+	
+	
+
+	
+
+}
---
+++ src/test/java/com/projectmanagement/AdminServiceTest.java
@@ -0,0 +1,72 @@
+package com.projectmanagement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.projectmanagement.entity.Admin;
+import com.projectmanagement.exceptions.ResourceNotFoundException;
+import com.projectmanagement.repo.AdminRepo;
+import com.projectmanagement.repo.EmployeeRepo;
+import com.projectmanagement.service.AdminService;
+import com.projectmanagement.service.EmployeeService;
+
+@SpringBootTest
+class AdminServiceTest {
+
+	@Autowired
+	private AdminService adminService;
+	@Autowired
+	private AdminRepo adminRepo;
+	
+	
+	@Test
+	  void testAddAdmin() {
+		Admin admin = new Admin();
+		admin.setUserId(1);
+		admin.setUserName("admin");
+		admin.setUserPassword("1234");
+		admin.setRole("ADMIN");
+		assertEquals(admin.toString(), adminService.addAdmin(admin).toString());
+		adminRepo.delete(admin);
+
+	}
+	
+	
+
+	@Test
+	void testUpdateAdmin() throws ResourceNotFoundException {
+		Admin admin = new Admin();
+		admin.setUserId(1);
+		admin.setUserName("admin");
+		admin.setUserPassword("1234");
+		admin.setRole("ADMIN");
+
+		adminRepo.save(admin);
+
+		// update admin
+
+		Admin uadmin = new Admin();
+		uadmin.setUserId(1);
+		uadmin.setUserName("admin");
+		uadmin.setUserPassword("root");
+		uadmin.setRole("ADMIN");
+		assertEquals(uadmin.toString(), adminService.updateAdmin(uadmin).toString());
+		adminRepo.delete(uadmin);
+	}
+
+	@Test
+	void testAuthenticateAdmin() throws Exception {
+		Admin admin = new Admin();
+		admin.setUserId(1);
+		admin.setUserName("admin");
+		admin.setUserPassword("1234");
+		admin.setRole("ADMIN");
+		adminRepo.save(admin);
+		assertEquals(admin.toString(), adminService.authenticateAdmin("admin", "1234").toString());
+		adminRepo.delete(admin);
+	}
+
+}
